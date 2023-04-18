e75f1f2ac67f62239d936705231af8f32bdbe738
ace.haddock <ace.haddock@gmail.com>
2023-04-17 19:53:21 -0500

    update index.ts

--- ColorfulOptionSet/index.ts
+++ ColorfulOptionSet/index.ts
@@ -3,6 +3,30 @@ import {IInputs, IOutputs} from "./generated/ManifestTypes";
 export class ColorfulOptionSet implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
     /**
+     * Variable Declaration
+     */
+    private theContext: ComponentFramework.Context<IInputs>;
+    private theNotifyChanged: () => void;
+    private theContainer: HTMLDivElement;
+
+    private _outputValue: number | null;
+    private _defaultValue: number | undefined;
+
+    private optionSetArray:
+        | ComponentFramework.PropertyHelper.OptionMetadata[]
+        | undefined;
+
+    /**
+     * HTML Elements
+     */
+
+    private eleMainContainer: HTMLDivElement;
+
+    /**
+     * Empty constructor.
+     */
+
+    /**
      * Empty constructor.
      */
     constructor()
@@ -21,6 +45,54 @@ export class ColorfulOptionSet implements ComponentFramework.StandardControl<IIn
     public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
     {
         // Add control initialization code
+        // Initialize Component variables
+    this.theContainer = container;
+    this.theContext = context;
+    this.theNotifyChanged = notifyOutputChanged;
+    context.mode.trackContainerResize(false);
+
+    // Get all options
+    this.optionSetArray =
+      context.parameters.OptionSetAttribute.attributes?.Options;
+
+    // Add code to update control view
+    this._defaultValue =
+      context.parameters.OptionSetAttribute.attributes?.DefaultValue;
+
+    // Current selected value
+    let currentInputData = context.parameters.OptionSetAttribute.raw || null;
+    if (currentInputData != null) {
+      this._outputValue = currentInputData;
+    } else {
+      if (this._defaultValue) {
+        this._outputValue = this._defaultValue;
+      }
+    }
+
+    // UI
+    // Main Container
+    this.eleMainContainer = document.createElement("div");
+    this.eleMainContainer.id = "mybtn";
+    this.eleMainContainer.className = "btn-group";
+
+    // Create OptionSet Buttons
+    if (this.optionSetArray) {
+      let width = 100 / this.optionSetArray.length;
+      for (var i = 0; i < this.optionSetArray.length; i++) {
+        // Button
+        let eleButton: HTMLButtonElement;
+        eleButton = document.createElement("button");
+        eleButton.innerHTML = this.optionSetArray[i].Label;
+        eleButton.id = this.optionSetArray[i].Value.toString();
+        eleButton.style.width = width.toString() + "%";
+        eleButton.style.background = "#6f6f6f";
+        eleButton.addEventListener("click", this.onButtonClick.bind(this));
+
+        this.eleMainContainer.appendChild(eleButton);
+      }
+    }
+    container.appendChild(this.eleMainContainer);
+
     }
 
 
@@ -31,6 +103,34 @@ export class ColorfulOptionSet implements ComponentFramework.StandardControl<IIn
     public updateView(context: ComponentFramework.Context<IInputs>): void
     {
         // Add code to update control view
+
+        if (this.eleMainContainer.children.length > 0) {
+            for (var i = 0; i < this.eleMainContainer.children.length; i++) {
+              let elementButton = this.eleMainContainer.children[
+                i
+              ] as HTMLButtonElement;
+      
+              if (
+                this._outputValue &&
+                elementButton.id !== this._outputValue.toString()
+              ) {
+                elementButton.style.background = "#6f6f6f";
+              } else if (
+                this._outputValue &&
+                elementButton.id === this._outputValue.toString()
+              ) {
+                if (this.optionSetArray) {
+                  var result = this.optionSetArray.filter((obj) => {
+                    return obj.Value === parseInt(elementButton.id);
+                  });
+                  elementButton.style.background = result[0].Color;
+                }
+              } else if (!this._outputValue) {
+                elementButton.style.background = "#6f6f6f";
+              }
+              elementButton.disabled = context.mode.isControlDisabled;
+            }
+          }
     }
 
     /**
@@ -39,8 +139,26 @@ export class ColorfulOptionSet implements ComponentFramework.StandardControl<IIn
      */
     public getOutputs(): IOutputs
     {
-        return {};
+        return {
+            OptionSetAttribute: this._outputValue == null ? -1 : this._outputValue,
+        };
     }
+
+    private onButtonClick(event: Event): void {
+        // Selected button details
+        let selectedElement = event.target as HTMLButtonElement;
+        if (this._defaultValue && this._defaultValue > -1) {
+          if (selectedElement) this._outputValue = parseInt(selectedElement.id);
+        } else {
+          if (selectedElement.id === this._outputValue?.toString()) {
+            this._outputValue = null;
+          } else {
+            if (selectedElement) this._outputValue = parseInt(selectedElement.id);
+          }
+        }
+        // if (selectedElement) this._outputValue = parseInt(selectedElement.id);
+        this.theNotifyChanged();
+      }
 
     /**
      * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
